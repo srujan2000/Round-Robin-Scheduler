d5cdee74647a9953a1246e7799bb1fad15d4d4e9
srujan2000 <srujanreddy168@gmail.com>
2025-11-25 22:22:00 +0530

    added stack operations

--- risc-v/main.s
+++ risc-v/main.s
@@ -8,19 +8,36 @@ main:
   csrs mstatus, t0
   li   t0, 0x80   # timer interrupt enable
   csrs mie, t0
-  la      t0, interrupt_handler #load interrupt handler address in mtvec
-  csrw    mtvec, t0 
-  li   t1, 0x80000000
-  add  sp, t0,t1  #setting up stack pointer
-  li      t0, 5000000
-  li      t1, 0x2004000  #mtimecmp address
-  sw      t0, 0(t1)
+  la   t0, interrupt_handler #load interrupt handler address in mtvec
+  csrw  mtvec, t0 
+  li    t0, 0x4000 # set mtime to 0x10000000
+  li    t1, 0x80000000
+  add   sp, t0,t1  #setting up stack pointer
+  li    t0, 5000000
+  li    t1, 0x2004000  #mtimecmp address
+  sw    t0, 0(t1)
 
 .section .text
 .type interrupt_handler, %function
 interrupt_handler:
     addi s1, s1,1  # timer interrupt enabled
     csrr t2, mcause
+    
+    addi t0,t0,4
+    addi t1,t1,8
+    addi t2,t2,12
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+    sw t2, 8(sp)
+    
+    add t0,x0,x0
+    add t1,x0,x0
+    add t2,x0,x0
+
+    lw t0,0(sp)
+    lw t1,4(sp)
+    lw t2,8(sp)
+    
 
     #push to the stack
     #pop the the stack
@@ -28,3 +45,6 @@ interrupt_handler:
     mret
 
 
+task1:
+    addi a0,a0,1
+    j task1
